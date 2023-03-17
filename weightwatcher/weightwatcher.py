fb021435dd8f9be43b18a32a29311c577dc19738
Charles H Martin, PhD <charlesmartin14@gmail.com>
2023-03-16 18:15:55 -0700

    trying to fix issue #221 and 137

--- weightwatcher/weightwatcher.py
+++ weightwatcher/weightwatcher.py
@@ -2658,7 +2658,7 @@ class WeightWatcher:
 
                 # issue 137
                 # details = details.append(ww_layer.get_row(), ignore_index=True)
-                print(ww_layer.get_row())
+                logger.debug(f"the layer is {str(ww_layer.get_row())}")
                 data = pd.DataFrame.from_records(ww_layer.get_row() , index=[0])
                 details = pd.concat([details,data], ignore_index=True)
 

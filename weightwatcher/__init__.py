8529e2554286da7c0fad4cdd72c7873b83675ffa
Charles H Martin, PhD <charlesmartin14@gmail.com>
2023-03-28 23:30:52 -0700

    debugging process pytorch binfiles

--- weightwatcher/__init__.py
+++ weightwatcher/__init__.py
@@ -18,7 +18,7 @@ from .weightwatcher import WeightWatcher
 
 
 __name__ = "weightwatcher"
-__version__ = "0.7.0.4"
+__version__ = "0.7.0.5"
 __license__ = "Apache License, Version 2.0"
 __description__ = "Diagnostic Tool for Deep Neural Networks"
 __url__ = "https://calculationconsulting.com/"
--- weightwatcher/weightwatcher.py
+++ weightwatcher/weightwatcher.py
@@ -4589,8 +4589,8 @@ class WeightWatcher:
                     logger.info(f"reading and extracting {state_dict_filename}")
                     # TODO:  update layer ids
                     layer_configs = WeightWatcher.extract_pytorch_statedict(weights_dir, model_name, state_dict_filename, start_id) 
-                    layer_ids = [x for x in layer_configs.keys()]
-                    start_id = start_id + np.max(layer_ids)+1
+                    last_id = int(layer_configs.keys()[-1])
+                    start_id = start_id +last_id + 1
                     config['layers'].update(layer_configs) 
                     logger.debug(f"num layer_ids {len(layer_ids)} last layer_id {start_id-1}")
                 

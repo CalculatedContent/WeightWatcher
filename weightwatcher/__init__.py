9d62553e51dc5a988a0721523512a06071bc0bdc
Charles H Martin, PhD <charlesmartin14@gmail.com>
2023-06-14 14:32:48 -0700

    base model support

--- weightwatcher/__init__.py
+++ weightwatcher/__init__.py
@@ -18,7 +18,7 @@ from .weightwatcher import WeightWatcher
 
 
 __name__ = "weightwatcher"
-__version__ = "0.7.1.9"
+__version__ = "0.7.2"
 __license__ = "Apache License, Version 2.0"
 __description__ = "Diagnostic Tool for Deep Neural Networks"
 __url__ = "https://calculationconsulting.com/"
